6ae396c078c5e3829dba7be7866f2e0a5bada469
Matthias Fuchs <mfuchs@ma-fu.de>
2019-02-03 17:37:08 +0100

    TMP: g3000-nand image generation
    Still little bit work in progress. For now we
    use zImage and a separate dtb file for booting and installation.
    
    Later we can switch to u-boot multi image or better FIT images.
    But this needs some U-Boot modifications.

--- target/linux/layerscape/image/armv7.mk
+++ target/linux/layerscape/image/armv7.mk
@@ -61,3 +61,24 @@ define Device/ls1021aiot-sdboot
     append-rootfs | check-size $(LS_SD_IMAGE_SIZE)
 endef
 TARGET_DEVICES += ls1021aiot-sdboot
+
+define Device/g3000-nand
+  DEVICE_TITLE := G3000 (NAND Boot)
+  DEVICE_DTS := g3000
+  FILESYSTEMS := ubifs
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  UBIFS_OPTS := -m 2048 -e 124KiB -c 4096
+  UBINIZE_OPTS := -E 5
+#  KERNELNAME:=uImage dtbs
+  KERNELNAME:=zImage dtbs
+  IMAGES := factory_ubi.bin factory_kernel.bin factory_dtb.bin sysupgrade.tar
+  KERNEL := kernel-bin # | gzip # | g3000-MuImage
+# TODO: factory_kernel.bin is zImage -> fine for inital boot, but ledenand needs uImage
+  IMAGE/factory_dtb.bin := ls-append-dtb $$(DEVICE_DTS) # dtb
+  IMAGE/factory_ubi.bin := append-ubi       # ubi'nized image
+  IMAGE/factory_kernel.bin := append-kernel # MuImage (kernel + fake.rd + dtb)
+#  IMAGE/factory_rootfs.bin := append-rootfs # ubifs partition image
+  IMAGE/sysupgrade.tar := sysupgrade-tar
+endef
+TARGET_DEVICES += g3000-nand
